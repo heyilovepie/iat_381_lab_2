889eddda13747e22a3d8aa12cdef68e370b22f75
Gillian Ramsay <gillianramsay@shaw.ca>
2015-01-19 12:34:34 -0800

    got it working

---
+++ app/scripts/controllers.js
@@ -0,0 +1,14 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+  $scope.phones = [
+    {'name': 'Nexus S5',
+     'snippet': 'Fast just got faster with Nexus S.'},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.'},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.'}
+  ];
+
+  $scope.text = 'hey';
+});
